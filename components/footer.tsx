2a285ef5a6ee9a65db47576bc7d24b5d9fb7c3c7
Clayton King <clayk@M1-MacBook-Pro.fios-router.home>
2023-07-17 14:11:35 -0400

    stripe webhook implemented

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <footer className='py-5 md:px-8 md:py-0 border-t'>
-      <div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row text-sm'>
-        <p className='text-center leading-loose text-muted-foreground md:text-left'>
+      <div className='flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row text-md'>
+        <p className='text-muted-foreground text-sm'>
           &copy; 2023 UrbanGoods. All rights reserved.
         </p>
       </div>
